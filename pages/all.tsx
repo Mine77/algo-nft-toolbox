7b13c1a10f507ead41a831f3c4c77e27a8e94c43
Mine77 <haichaozhu77@gmail.com>
2022-01-06 22:22:02 +0800

    try

--- pages/all.tsx
+++ pages/all.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import NFTCard from "../components/nftCard";
 import Layout from "components/layout";
 import { useContext } from "react";
 import AlertContext from "components/context/alertContext";
@@ -16,13 +15,13 @@ export const Events: NextPage = () => {
         </div>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-12">
-        <NFTCard
+        {/* <NFTCard
           Standard="ARC3"
           Name="All-Hands 2021 Dec"
           Description="Attendance proof of Algorand Community All-Hands December 2021."
           NFTID="450090741"
           CreatedTime="12/1/2021"
-        />
+        /> */}
       </div>
     </Layout>
   );
