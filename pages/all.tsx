25185d13714226676fe548af5092d67793149139
Mine77 <haichaozhu77@gmail.com>
2022-01-06 22:56:02 +0800

    delete nftcard

--- pages/all.tsx
+++ pages/all.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import Layout from "components/layout";
 import { useContext } from "react";
 import AlertContext from "components/context/alertContext";
-import NftCard from "components/nftCard";
 
 export const Events: NextPage = () => {
   const alert = useContext(AlertContext);
@@ -15,13 +14,6 @@ export const Events: NextPage = () => {
         </div>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-12">
-        <NftCard
-          Standard="ARC3"
-          Name="All-Hands 2021 Dec"
-          Description="Attendance proof of Algorand Community All-Hands December 2021."
-          NFTID="450090741"
-          CreatedTime="12/1/2021"
-        />
       </div>
     </Layout>
   );
